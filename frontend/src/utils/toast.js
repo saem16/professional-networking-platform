ad5d0d17c52e1eafc03802673c6ea6c6d0767af7
saem16 <131770602+saem16@users.noreply.github.com>
2025-09-20 15:17:06 +0530

    Delete frontend directory

--- frontend/src/utils/toast.js
+++
@@ -1,9 +0,0 @@
-import { toast } from 'react-hot-toast';
-
-export const showToast = {
-  success: (message) => toast.success(message),
-  error: (message) => toast.error(message),
-  loading: (message) => toast.loading(message),
-  dismiss: (toastId) => toast.dismiss(toastId),
-  promise: (promise, messages) => toast.promise(promise, messages)
-};
